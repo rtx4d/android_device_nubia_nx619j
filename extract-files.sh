5e007afdfdc88844ee725ef77861dbf3fa92ef64
Bruno Martins <bgcngm@gmail.com>
2020-12-28 10:34:04 +0000

    nx619j: Align with extraction-utils templates
    Change-Id: Ifbf18f79a1d0f173cb5b83bdf0178db2a5f1373e

--- extract-files.sh
+++ extract-files.sh
@@ -24,11 +24,8 @@ fi
 
 set -e
 
-# Required!
 export DEVICE=nx619j
 export DEVICE_COMMON=sdm845-common
 export VENDOR=nubia
-
-export DEVICE_BRINGUP_YEAR=2023
 
 "./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"

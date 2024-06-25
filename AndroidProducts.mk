c7ad69d1a02ab2a36a89e3fdd9307047da766506
Michael Bestas <mkbestas@lineageos.org>
2024-06-26 00:13:14 +0700

    nx619j: Drop COMMON_LUNCH_CHOICES
    As of 14 QPR2 this is broken, let's remove it instead of updating it
    every 3 months. breakfast is the way to go.
    
    Change-Id: I097bd54b894d9a49c7ce8b1ab6f343349e268fd8

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,8 @@
 #
-# Copyright (C) 2018-2023 The LineageOS Project
+# Copyright (C) 2018-2024 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_nx619j.mk
-
-COMMON_LUNCH_CHOICES := \
-    lineage_nx619j-user \
-    lineage_nx619j-userdebug \
-    lineage_nx619j-eng

9a48e5fdca79d62ab6ea0465a54106b57ae99fd3
Michael Bestas <mkbestas@lineageos.org>
2024-10-26 18:35:18 +0700

    nx619j: Sync extract scripts with templates
    Change-Id: I98a36854c5a0c0390592911bb2558b34eff77efc

--- extract-files.sh
+++ extract-files.sh
@@ -1,26 +1,35 @@
 #!/bin/bash
 #
-# Copyright (C) 2018-2023 The LineageOS Project
-#
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
 
 function blob_fixup() {
     case "${1}" in
-    vendor/lib/hw/audio.primary.sdm845.so)
-        "${PATCHELF}" --replace-needed "libcutils.so" "libprocessgroup.so" "${2}"
-        ;;
-    vendor/lib64/hw/audio.primary.sdm845.so)
-        "${PATCHELF}" --replace-needed "libcutils.so" "libprocessgroup.so" "${2}"
-        ;;
-    vendor/lib64/libgoodixhwfingerprint.so)
-        "${PATCHELF}" --remove-needed "libkeymaster_messages.so" "${2}"
-        "${PATCHELF}" --remove-needed "libsoftkeymasterdevice.so" "${2}"
-        ;;
-    vendor/lib64/vendor.goodix.hardware.biometrics.fingerprint@2.1.so)
-        "${PATCHELF}" --replace-needed "libhidlbase.so" "libhidlbase-v32.so" "${2}"
-        ;;
+        vendor/lib/hw/audio.primary.sdm845.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --replace-needed "libcutils.so" "libprocessgroup.so" "${2}"
+            ;;
+        vendor/lib64/hw/audio.primary.sdm845.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --replace-needed "libcutils.so" "libprocessgroup.so" "${2}"
+            ;;
+        vendor/lib64/libgoodixhwfingerprint.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --remove-needed "libkeymaster_messages.so" "${2}"
+            "${PATCHELF}" --remove-needed "libsoftkeymasterdevice.so" "${2}"
+            ;;
+        vendor/lib64/vendor.goodix.hardware.biometrics.fingerprint@2.1.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --replace-needed "libhidlbase.so" "libhidlbase-v32.so" "${2}"
+            ;;
+        *)
+            return 1
+            ;;
     esac
+
+    return 0
 }
 
 # If we're being sourced by the common script that we called,
@@ -34,5 +43,6 @@ set -e
 export DEVICE=nx619j
 export DEVICE_COMMON=sdm845-common
 export VENDOR=nubia
+export VENDOR_COMMON=${VENDOR}
 
-"./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
+"./../../${VENDOR_COMMON}/${DEVICE_COMMON}/extract-files.sh" "$@"
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 #
-# Copyright (C) 2018-2023 The LineageOS Project
-#
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
+
+# If we're being sourced by the common script that we called,
+# stop right here. No need to go down the rabbit hole.
+if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
+    return
+fi
 
 set -e
 
 export DEVICE=nx619j
 export DEVICE_COMMON=sdm845-common
 export VENDOR=nubia
+export VENDOR_COMMON=${VENDOR}
 
-"./../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"
+"./../../${VENDOR_COMMON}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"

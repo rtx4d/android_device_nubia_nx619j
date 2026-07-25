cde9fdd91f39a3960c81ea0ed4c7295d68bd776b
Andrey Sprynga <rtx4d@aospa.co>
2026-07-25 23:25:36 +0700

    nx619j: Optimize extract-files.py script
    Change-Id: I53ba8fbd1ee3f2f74eb7e3624df805d5c7b3752c

--- extract-files.py
+++ extract-files.py
@@ -32,20 +32,14 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/lib64/libgoodixhwfingerprint.so': blob_fixup()
         .remove_needed('libkeymaster_messages.so')
         .remove_needed('libsoftkeymasterdevice.so'),
-    'vendor/lib64/vendor.goodix.hardware.biometrics.fingerprint@2.1.so': blob_fixup()
+    ('vendor/lib64/vendor.goodix.hardware.biometrics.fingerprint@2.1.so',  'vendor/lib64/vendor.nubia.ifaa@1.0.so', 'vendor/lib64/vendor.qti.hardware.fingerprint@1.0.so') : blob_fixup()
         .replace_needed('libhidlbase.so', 'libhidlbase-v32.so'),
-    'vendor/lib/libarcsoft_beautyshot_video_algorithm.so': blob_fixup()
+    ('vendor/lib/libarcsoft_beautyshot_video_algorithm.so', 'vendor/lib/libswregistrationalgo.so') : blob_fixup()
         .replace_needed('libstdc++.so', 'libstdc++_vendor.so'),
-    'vendor/lib/libswregistrationalgo.so': blob_fixup()
-        .replace_needed('libstdc++.so', 'libstdc++_vendor.so'),
-    'vendor/lib64/vendor.nubia.ifaa@1.0.so': blob_fixup()
-        .replace_needed('libhidlbase.so', 'libhidlbase-v32.so'),
     'vendor/lib/libmmcamera_faceproc.so': blob_fixup()
         .clear_symbol_version('__aeabi_memcpy')
         .clear_symbol_version('__aeabi_memset')
         .clear_symbol_version('__gnu_Unwind_Find_exidx'),
-    'vendor/lib64/vendor.qti.hardware.fingerprint@1.0.so': blob_fixup()
-        .replace_needed('libhidlbase.so', 'libhidlbase-v32.so'),
     'vendor/lib/camera/components/com.nubia.node.realtimeaicamera.so': blob_fixup()
         .add_needed('libcutils.so'),
 }  # fmt: skip
